1011a3954c52bef21389a2ec73975abe3dd39a3b
AValeriya <79207820+AValeriya@users.noreply.github.com>
2023-05-23 14:47:26 +0300

    Delete script.js

--- travel/script.js
+++
@@ -1,5 +0,0 @@
-let searchBtn = document.querySelector('#search-btn');
-let searchBar = document.querySelector('.search-bar-container');
-
-searchBtn.addEventListener('click', () =>)
-                           
